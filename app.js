ac38b4a9156eeb2a6a552dd40db0ea5e1e0f1a09
JanBoeker <naj_b@gmx.de>
2020-11-28 22:00:30 +0100

    finish todolist app

--- app.js
+++ app.js
@@ -2,37 +2,50 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 const app = express();
 
-var items = [];
+const items = ["buy food", "cook food", "eat food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Staitsche Dateien für die server based WebApp
+app.use(express.static("public"));
+
+
 app.get("/", function(req, res) {
 
-  var today = new Date();
-  var options = {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric'
-  };
-
-  var day = today.toLocaleDateString('en-US', options);
-
   res.render("list.ejs", {
-    kindOfDay: day,
+    listTitle: date.getDate(),
     newListItems: items
   });
 
 });
 
 app.post("/", function(req, res) {
-  items.push(req.body.newItem);
-  res.redirect("/");
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
 });
 
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work", newListItems: workItems})
+});
+
+app.get("/about", function(req, res) {
+  res.render("about.ejs");
+});
 
 app.listen(3000, function() {
   console.log("Server running on port 3000");
